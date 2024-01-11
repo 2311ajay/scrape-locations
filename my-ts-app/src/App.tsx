2b3f32f00c514837e342454a19a723e354a8bdbb
2311ajay <92317294+2311ajay@users.noreply.github.com>
2024-01-11 20:38:15 +0800

    Cleaned up some files, added tool tips to map

--- my-ts-app/src/App.tsx
+++ my-ts-app/src/App.tsx
@@ -1,18 +1,31 @@
 import React, { useEffect, useRef, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Circle, CircleMarker, MapContainer, Marker, Polygon, Polyline, Popup, Rectangle, TileLayer } from 'react-leaflet';
+import { Circle, MapContainer, Marker, TileLayer, Tooltip } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from "leaflet";
 import IconPng from "./images/pin.png";
-import { idText } from 'typescript';
+
 
 interface StoreLocation {
   store_name: String;
   store_location: String;
-  store_geolocation: String;
+  store_geolocation: [number, number]; //[lat, long]
   id: number;
   created_at: String,
+}
+
+const degToRad = (deg: number): number => {
+  return deg * (Math.PI / 180.0);
+};
+
+const distanceBetweenStores = (s1: StoreLocation, s2: StoreLocation) => {
+    const d = Math.acos(
+                        (Math.sin(degToRad(s1.store_geolocation[0])) * Math.sin(degToRad(s2.store_geolocation[0]))) + 
+                        (Math.cos(degToRad(s1.store_geolocation[0])) * Math.cos(degToRad(s2.store_geolocation[0]))) 
+                        
+                        * (Math.cos(degToRad(s2.store_geolocation[1]) - degToRad(s1.store_geolocation[1])))) * 6371
+
+    return d;
 }
 
 function App() {
@@ -26,15 +39,9 @@ function App() {
 });
   
   const mapRef = useRef();
-  const POINT_SIZE = 10;
-  const RADIUS_OF_NETWORK = 5000;
-  const [pointRadius, setPointRadius] = useState(POINT_SIZE);
-  const [boundaryRadius, setBoundaryRadius] = useState(POINT_SIZE);
-  const fillBlueOptions = { fillColor: 'blue' }
-  const blackOptions = { color: 'black' }
+  const RADIUS_OF_NETWORK = 4000;
   const limeOptions = { color: 'lime' }
-  const purpleOptions = { color: 'purple' }
-  const redOptions = { color: 'red' }
+  const greyOptions = { color: 'grey' }
 
   useEffect(() => {
     fetch('http://127.0.0.1:8000/api/tasks/')
@@ -43,7 +50,18 @@ function App() {
       })
       .then((data) => {
         console.log("scraped data =", data);
-        setStoreLocations(data);
+        const processedData = data.map((d: { store_geolocation: any; }) => {
+          let geoLoc = d.store_geolocation;
+          geoLoc = geoLoc.replace("(", "");
+          geoLoc = geoLoc.replace(")", "");
+          const latLong = geoLoc.split(",");
+          const position: [number, number] = [latLong[0], latLong[1]];
+          return {
+            ...d,
+            store_geolocation: position
+          }
+        })
+        setStoreLocations(processedData);
       })
       .catch(
         e => console.log("error =", e)
@@ -52,85 +70,34 @@ function App() {
 
   return (
     // @ts-ignore
-    <MapContainer center={[2.200844, 102.240143]} zoom={13} scrollWheelZoom={false} ref={mapRef}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {/* <Marker position={center} icon={myIcon} />
-      <Circle radius={RADIUS_OF_NETWORK} center={center} /> */}
-      {storeLocations.map(storeLocation => {
-        let geoLoc = storeLocation.store_geolocation;
-        geoLoc = geoLoc.replace("(", "");
-        geoLoc = geoLoc.replace(")", "");
-        const latLong = geoLoc.split(",");
-        const position: [number, number] = [+latLong[0], +latLong[1]];
-        const inNetwork = true;
-        return (
-          <>
-            <Marker position={position} icon={myIcon} />
-            <Circle radius={RADIUS_OF_NETWORK} center={position} pathOptions={inNetwork? limeOptions: redOptions}/>
-          </>
-        )})}
-        {/* <Polyline pathOptions={limeOptions} positions={polyline} />
-        <Polyline pathOptions={limeOptions} positions={multiPolyline} />
-        <Polygon pathOptions={purpleOptions} positions={polygon} />
-        <Polygon pathOptions={purpleOptions} positions={multiPolygon} />
-        <Rectangle bounds={rectangle} pathOptions={blackOptions} /> */}
-    </MapContainer>
+    <div className='App'>
+      {/* @ts-ignore */}
+      <MapContainer center={center} zoom={13} scrollWheelZoom={false} ref={mapRef}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {storeLocations.map(storeLocation => {
+          let geoLoc = storeLocation.store_geolocation;
+          const position: [number, number] = [geoLoc[0], geoLoc[1]];
+          let withinANetwork = false;
+          storeLocations.forEach(element => {
+            if (distanceBetweenStores(element, storeLocation) <= 5 && element.id !== storeLocation.id ) {
+              withinANetwork = true
+            }
+          });
+
+          return (
+            <>
+              <Marker position={position} icon={myIcon} />
+              <Circle radius={RADIUS_OF_NETWORK} center={position} pathOptions={withinANetwork? limeOptions: greyOptions}>
+                <Tooltip sticky>{storeLocation.store_name}</Tooltip>
+              </Circle>
+            </>
+          )})}
+      </MapContainer>
+    </div>
   );
 }
 
 export default App;
-
-// import L from "leaflet";
-// import { MapContainer, TileLayer, Marker, Circle } from "react-leaflet";
-// import { useState, useRef, useEffect } from "react";
-// import IconPng from "./images/pin.png";
-
-// const myIcon = L.icon({
-//   iconUrl: IconPng,
-//   iconSize: [35, 43],
-//   iconAnchor: [18, 26]
-// });
-
-// const center = {
-//   lat: 51.505,
-//   lng: -0.09
-// };
-
-// function DroppableMarker({ pos, radius }: {pos: [number, number], radius: number}) {
-//   return (
-//     <>
-//       <Marker position={pos} icon={myIcon} />
-//       <Circle radius={radius} center={pos} />
-//     </>
-//   );
-// }
-
-// export default function App() {
-//   const mapRef = useRef();
-//   const [position, setPosition] = useState(null);
-//   const [radius, setRadius] = useState(1000);
-
-//   // @ts-ignore
-//   const addMarker = (e) => {
-
-//     setPosition([e.latlng.lat, e.latlng.lng]);
-
-//     setRadius(1000);
-    
-//     console.log("zoom", mapRef.current.leafletElement.getZoom());
-//   };
-
-//   return (
-//     // @ts-ignore
-//     <MapContainer center={center} zoom={13} tap={false} onClick={addMarker} ref={mapRef}>
-//       <TileLayer
-//         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//       />
-//       {position ? <DroppableMarker pos={position} radius={radius} /> : null}
-//     </MapContainer>
-//   );
-// }
